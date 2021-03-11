1ec2a9d0a64b8b5d7f808865ca0f16b48eda7867
Juanjo Torres <jjtr96@gmail.com>
2021-03-11 21:41:15 +0100

    Configuration of the environment.

---
+++ Assets/Scripts/DemoImporterStl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using Assimp;
+
+public class DemoImporterStl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        AssimpContext importer = new AssimpContext();
+
+        Stream stream = File.Open("mymodel.fbx", FileMode.Open);
+        Scene scene = importer.ImportFileFromStream(stream);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
