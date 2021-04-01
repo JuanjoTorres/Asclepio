c358eee2bde8bbebcb0aee2ec34aa26f5b325b5d
Juanjo Torres <jjtr96@gmail.com>
2021-04-01 17:14:21 +0200

    Example commit

--- Assets/Scripts/DemoImporterStl.cs
+++ Assets/Scripts/DemoImporterStl.cs
@@ -1,31 +1,71 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using UnityEngine;
+using UnityEditor;
 using System.IO;
-using UnityEngine;
-using UnityMeshImporter;
+using System.Linq;
 
-public class DemoImporterStl : MonoBehaviour
+namespace Parabox.Stl.Editor
 {
-    private const string MeshPath = "C:/Users/Juanjo Torres/Documents/repos/unity/Asclepio/Assets/Resources/smoothPelvis.stl";
-    private GameObject Workspace;
-    // Start is called before the first frame update
-
-    private void Awake()
+    public class DemoImporterStl : MonoBehaviour
     {
-        Workspace = GameObject.Find("Workspace");
-        // string fileName = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "smoothPelvis.stl");
-        string fileName = Path.Combine(Application.dataPath, "cube2.stl");
-        print(fileName);
+        private const string MeshPath = "C:/Users/Juanjo Torres/Desktop/pelvis-ascii.stl";
+        // private GameObject Workspace;
+        // Start is called before the first frame update
 
-        var newAsset = MeshImporter.Load(MeshPath);
-        print(Workspace.name);
-        newAsset.transform.SetParent(Workspace.transform, false);
-    }
+        private void Awake()
+        {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            Debug.Log("Hello!!");
+            var name = Path.GetFileNameWithoutExtension(MeshPath);
+            print(name);
+            var meshes = Importer.Import(MeshPath);
+
+            if (meshes.Length < 1)
+                return;
+
+            if (meshes.Length < 2)
+            {
+                var go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+                Object.DestroyImmediate(go.GetComponent<BoxCollider>());
+                go.name = name;
+                meshes[0].name = "Mesh-" + name;
+                go.GetComponent<MeshFilter>().sharedMesh = meshes[0];
+
+                AssetDatabase.CreateAsset(go, go.name);
+                AssetDatabase.AddObjectToAsset(meshes[0], meshes[0].name);
+                AssetDatabase.SetMainObject(go, go.name);
+
+            }
+            else
+            {
+                var parent = new GameObject();
+                parent.name = name;
+
+                for (int i = 0, c = meshes.Length; i < c; i++)
+                {
+                    var go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                    Object.DestroyImmediate(go.GetComponent<BoxCollider>());
+                    go.transform.SetParent(parent.transform, false);
+                    go.name = name + "(" + i + ")";
+
+                    var mesh = meshes[i];
+                    mesh.name = "Mesh-" + name + "(" + i + ")";
+                    go.GetComponent<MeshFilter>().sharedMesh = mesh;
+
+                    AssetDatabase.AddObjectToAsset(mesh, mesh.name);
+                }
+
+                AssetDatabase.AddObjectToAsset(parent, parent.name);
+                AssetDatabase.SetMainObject(parent, parent.name);
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
     }
 
 }
