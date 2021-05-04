636b56c327facda95fdb72c42b6fc4948cec20e5
Juanjo Torres <jjtr96@gmail.com>
2021-05-04 23:34:58 +0200

    Añadiendo un segundo boton para poder habilitar/deshabilitar la opcion de teletransporte.

---
+++ Assets/LocomotionController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class LocomotionController : MonoBehaviour
+{
+    public XRController rightTeleportRay;
+    public InputHelpers.Button teleportActivationButton;
+    public float activationThreshold = 0.1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Si existe el puntero de la mano derecha...
+        if (rightTeleportRay)
+            // mira si se ha activado el boton del teleport
+            rightTeleportRay.gameObject.SetActive(CheckIfActivated(rightTeleportRay));
+    }
+
+    // Verificar si se ha activado el boton
+    public bool CheckIfActivated(XRController controller)
+    {
+        InputHelpers.IsPressed(controller.inputDevice, 
+            teleportActivationButton, 
+            out bool isActivated, 
+            activationThreshold);
+
+        return isActivated;
+    }
+}
