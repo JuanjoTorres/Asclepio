2254ffd6826aac05676751ac7c676ce042c43b53
Juanjo Torres <jjtr96@gmail.com>
2021-06-18 21:35:37 +0200

    Scripts y materiales para el asset de la pistola (ejemplo de futura herramienta).

---
+++ Assets/Scripts/GunController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour
+{
+    public float speed = 40;
+    public GameObject bullet;
+    public Transform barrel;
+    public AudioSource audioSource;
+    public AudioClip audioClip;
+
+    public void Fire()
+    {
+        GameObject spawnedBullet = Instantiate(bullet, barrel.position, barrel.rotation);
+        spawnedBullet.GetComponent<Rigidbody>().velocity = speed * barrel.forward;
+        print(spawnedBullet.GetComponent<Rigidbody>().velocity);
+        audioSource.PlayOneShot(audioClip);
+        Destroy(spawnedBullet, 2);
+    }
+}
