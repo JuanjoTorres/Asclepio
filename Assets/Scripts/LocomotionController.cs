760f9bce59a875f6fa1c7d633d278b712e74b4b6
Juanjo Torres <jjtr96@gmail.com>
2021-06-18 21:36:35 +0200

    Debug del raycast mientras cogias un objeto.

--- Assets/Scripts/LocomotionController.cs
+++ Assets/Scripts/LocomotionController.cs
@@ -9,13 +9,15 @@ public class LocomotionController : MonoBehaviour
     public InputHelpers.Button teleportActivationButton;
     public float activationThreshold = 0.1f;
 
+    public bool EnableRightTeleport { get; set; } = true;
+
     // Update is called once per frame
     void Update()
     {
         // Si existe el puntero de la mano derecha...
         if (rightTeleportRay)
             // mira si se ha activado el boton del teleport
-            rightTeleportRay.gameObject.SetActive(CheckIfActivated(rightTeleportRay));
+            rightTeleportRay.gameObject.SetActive( EnableRightTeleport && CheckIfActivated(rightTeleportRay));
     }
 
     // Verificar si se ha activado el boton
