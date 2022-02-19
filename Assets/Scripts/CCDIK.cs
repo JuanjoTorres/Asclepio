79973def9543034cb18ca0f4bf96eba6673bcbd8
= <=>
2022-02-19 22:39:09 +0100

    no message

---
+++ Assets/Scripts/CCDIK.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class CCDIK : MonoBehaviour
+{
+    public Transform Tooltip;
+    public Transform Target;
+    public CCDIKJoint[] joints;
+
+    void Update()
+    {
+        for (int j = 0; j < joints.Length; j++)
+        {
+            joints[j].Evaluate(Tooltip, Target, j < 2);
+        }
+    }
+}
