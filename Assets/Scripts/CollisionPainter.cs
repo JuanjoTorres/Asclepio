a89fea6f269bb4881d83829820274d082b93ec74
= <=>
2022-02-26 21:31:46 +0100

    Implementando la herramienta de pintado.

---
+++ Assets/Scripts/CollisionPainter.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class CollisionPainter : MonoBehaviour
+{
+    public Color paintColor;
+
+    public float radius = 1;
+    public float strength = 1;
+    public float hardness = 1;
+
+    private void OnCollisionStay(Collision other)
+    {
+        Paintable p = other.collider.GetComponent<Paintable>();
+        if (p != null)
+        {
+            Debug.Log("Soy pintable!");
+            Debug.Log("radius: " + radius);
+            Debug.Log("hardness: " + hardness);
+            Debug.Log("strength: " + strength);
+            Debug.Log("paintColor: " + paintColor);
+
+            Vector3 pos = other.contacts[0].point;
+            PaintManager.instance.paint(p, pos, radius, hardness, strength, paintColor);
+        }
+    }
+}
---
+++ Assets/Scripts/FollowTarget.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class FollowTarget : MonoBehaviour
+{
+    public Transform target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localPosition = target.localPosition;
+        transform.rotation = target.rotation;
+    }
+}
